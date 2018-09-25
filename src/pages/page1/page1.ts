400f779e2cec2f0122552d6299d8a145009db37f
Evelas <Evelas@gmail.com>
2018-09-25 09:12:04 -0500

    second commit

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -9,13 +9,15 @@ import { Page2 } from '../page2/page2';
 import { LoginPage } from '../login/page/page';
 import { mapPage } from '../map/map/map';
 
+import {LoadingController} from 'ionic-angular';
+
 @Component({
     selector: 'page-page1',
     templateUrl: 'page1.html'
 })
 export class Page1 {
 
-    constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, public loadingCtrl:LoadingController) {
     }
 
     onLink(url: string) {
@@ -25,8 +27,18 @@ export class Page1 {
     	this.navCtrl.push(Page2);
     }
     openLoginPage() {
+		let loader = this.loadingCtrl.create({
+			spinner: "bubbles",
+			content: "Wait a minute",
+			duration: 1000
+});
+		loader.present();
+
+
       this.navCtrl.push(LoginPage);
   	}
+
+
   	openMapPage() {
       this.navCtrl.push(mapPage);
   	}
